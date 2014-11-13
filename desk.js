46323f24b9cd0379fba57b2c410aa6388db626e1
Jeremy Hall <jeremy@whiterabbitjapan.com>
2014-11-13 18:31:58 +0900

    Add initial function to test integration.

--- desk.js
+++ desk.js
@@ -1,1 +1,44 @@
-// Write your package code here!
+DeskAPI = DeskAPI || {};
+
+var directRequest = Npm.require("request");
+var oauth = Npm.require("oauth-1.0a");
+
+DeskAPI = function(){
+
+  var self = this;
+
+  var request = Meteor.wrapAsync(directRequest, self);
+
+  function token(){
+    return {
+      public: Meteor.settings.desk.token,
+      secret: Meteor.settings.desk.tokenSecret
+    };
+  }
+
+  var oauth = OAuth({
+    consumer: {
+      public: Meteor.settings.desk.key,
+      secret: Meteor.settings.desk.secret
+    },
+    signature_method: 'HMAC-SHA1'
+  });
+
+
+
+  self.getCases = function(){
+    var requestData = {
+      url: 'https://yoursite.desk.com/api/v2/cases',
+      method: 'GET'
+    };
+
+    var result = request({
+      url: requestData.url,
+      method: requestData.method,
+      form: oauth.authorize(requestData, token())
+    });
+
+    return result;
+  }
+}
+
--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'whiterabbit:desk',
   summary: 'A meteor wrapper for desk.com helpdesk API',
-  version: '0.1.1',
+  version: '0.1.2',
   git: 'https://github.com/white-rabbit-japan/meteor-desk'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('desk.js');
+  api.export('DeskAPI', 'server');
 });
 
 Npm.depends({
